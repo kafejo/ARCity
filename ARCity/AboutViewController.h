aef92a15a7fa04e6b3bcd060903b4b858b2f5e64
Ales Kocur <aleskocur@icloud.com>
2015-05-14 09:33:02 +0200

    About fix

---
+++ ARCity/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  ARCity
+//
+//  Created by Aleš Kocur on 14/05/15.
+//  Copyright (c) 2015 metaio GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ ARCity/AboutViewController.m
@@ -0,0 +1,42 @@
+//
+//  AboutViewController.m
+//  ARCity
+//
+//  Created by Aleš Kocur on 14/05/15.
+//  Copyright (c) 2015 metaio GmbH. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+- (IBAction)dismiss:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
