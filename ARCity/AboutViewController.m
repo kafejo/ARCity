a387e0ab207a19089b7d3c308ef5f390b307cb3f
Ales Kocur <aleskocur@icloud.com>
2015-05-14 10:00:24 +0200

    Fix about dismiss

--- ARCity/AboutViewController.m
+++ ARCity/AboutViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismiss:)];
+    [self.view addGestureRecognizer:tap];
 }
 
 - (void)didReceiveMemoryWarning {
